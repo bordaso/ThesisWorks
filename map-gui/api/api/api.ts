9f7845615f540ea11a51a6312c730e91bebb5da2
bordaso <zedasgm@gmail.com>
2022-05-21 22:22:26 +0200

    removed map-gui

--- map-gui/api/api/api.ts
+++
@@ -1,5 +0,0 @@
-export * from './basicErrorController.service';
-import { BasicErrorControllerService } from './basicErrorController.service';
-export * from './grpcTestRestController.service';
-import { GrpcTestRestControllerService } from './grpcTestRestController.service';
-export const APIS = [BasicErrorControllerService, GrpcTestRestControllerService];
